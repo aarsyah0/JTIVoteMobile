2e5c990dd70ac9213c444ab9b95758352661ae57
aarsyah0 <arsasaja2@gmail.com>
2023-12-07 23:10:32 +0700

    Create ElectionDetailsActivity.java

---
+++ Android 2/app/src/main/java/com/votee/voteee/ElectionDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.votee.voteee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ElectionDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_election_details);
+    }
+}
